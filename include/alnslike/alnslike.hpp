b1a4e11a1cd8735cb92e60fde72387fce81ce52e
Fraart <sir.kobzevs@yandex.com>
2026-09-06 09:58:15 +0700

    Add umbrella header

---
+++ include/alnslike/alnslike.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <alnslike/core/acceptance_criterion.hpp>
+#include <alnslike/core/alns_step.hpp>
+#include <alnslike/core/local_step.hpp>
+#include <alnslike/core/operator.hpp>
+#include <alnslike/core/operator_selector.hpp>
+#include <alnslike/core/post_processor.hpp>
+#include <alnslike/core/search_step.hpp>
+#include <alnslike/core/solution.hpp>
+#include <alnslike/core/solution_generator.hpp>
+#include <alnslike/core/solver.hpp>
+#include <alnslike/core/types.hpp>
