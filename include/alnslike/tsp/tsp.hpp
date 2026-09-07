5a63245b9f8fbe96b4367720e4044e74339811f8
Fraart <sir.kobzevs@yandex.com>
2026-09-07 20:46:21 +0700

    Add umbrella header

---
+++ include/alnslike/tsp/tsp.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <alnslike/tsp/coordinate.hpp>
+#include <alnslike/tsp/tsp_instance.hpp>
+#include <alnslike/tsp/tsp_solution.hpp>
+#include <alnslike/tsp/nearest_neighbor_generator.hpp>
+#include <alnslike/tsp/tsp_2opt_post_processor.hpp>
+
+// операторы
+#include <alnslike/tsp/operators/2opt.hpp>
+#include <alnslike/tsp/operators/3opt.hpp>
